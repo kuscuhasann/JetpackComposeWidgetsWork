3c29257524f58ba53853e83c4ea295f3a8d58f66
kuscuhasann <kuscuhasann97@gmail.com>
2022-08-24 14:13:14 +0300

    Slider ile çalışmak

---
+++ app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/SliderWorkPage.kt
@@ -0,0 +1,29 @@
+package com.pakt_games.jetpackcomposewidgetswork
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun SliderWorkPage() {
+    val valueOfSlider = remember {
+        mutableStateOf(0f)
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Slider Değeri: " + valueOfSlider.value)
+
+        Slider(value = valueOfSlider.value, onValueChange = {valueOfSlider.value = it} )
+    }
+}
