4aa233151f24206c00b808ee42b63633b5617f49
kuscuhasann <kuscuhasann97@gmail.com>
2022-08-24 21:00:44 +0300

    Top App Bar actions kullanımı

--- app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/TopAppBarWorkPage.kt
+++ app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/TopAppBarWorkPage.kt
@@ -1,15 +1,27 @@
 package com.pakt_games.jetpackcomposewidgetswork
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.clickable
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun TopAppBarWorkPage() {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Google")})
+            TopAppBar(
+                title = { Text(text = "Google")},
+                actions = {
+                    Text(text = "Exit", modifier = Modifier.clickable {
+                    System.exit(1)
+                    }
+                )
+                    IconButton(onClick = {  }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_baseline_add_24), contentDescription = "")
+                    }
+                }
+            )
         },
         content = {
 
