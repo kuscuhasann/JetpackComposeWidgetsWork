27ca6f0d1eb2fd405ec1ea6dc7a9414a057cf5a2
kuscuhasann <kuscuhasann97@gmail.com>
2022-08-24 12:56:57 +0300

    Switch ile çalışmak

---
+++ app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/SwitchWorkPage.kt
@@ -0,0 +1,28 @@
+package com.pakt_games.jetpackcomposewidgetswork
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SwitchWorkPage() {
+    var switchStatus = remember {
+        mutableStateOf(false)
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch(checked = switchStatus.value, onCheckedChange = {switchStatus.value = it} )
+        Text(text ="Switchin son durumu: " + switchStatus.value)
+    }
+}
