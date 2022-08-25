d9aebdf1620428e555aa8830fba43d4803790205
kuscuhasann <kuscuhasann97@gmail.com>
2022-08-25 09:32:57 +0300

    TopAppBar and DropDownList work

--- app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/TopAppBarWorkPage.kt
+++ app/src/main/java/com/pakt_games/jetpackcomposewidgetswork/TopAppBarWorkPage.kt
@@ -3,11 +3,17 @@ package com.pakt_games.jetpackcomposewidgetswork
 import androidx.compose.foundation.clickable
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 
 @Composable
 fun TopAppBarWorkPage() {
+    val menuOpeningControl = remember {
+        mutableStateOf(false)
+    }
+    val menuItemList = listOf("Şeker", "Tuz","Leblebi")
     Scaffold(
         topBar = {
             TopAppBar(
@@ -17,8 +23,18 @@ fun TopAppBarWorkPage() {
                     System.exit(1)
                     }
                 )
-                    IconButton(onClick = {  }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_baseline_add_24), contentDescription = "")
+                    IconButton(onClick = {
+                        menuOpeningControl.value = true
+                    }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_baseline_menu_24), contentDescription = "")
+                    }
+
+                    DropdownMenu(expanded = menuOpeningControl.value, onDismissRequest = { menuOpeningControl.value = false }) {
+                        menuItemList.forEachIndexed { index, menuItemName ->
+                            DropdownMenuItem(onClick = { menuOpeningControl.value }) {
+                                Text(text = menuItemName)
+                            }
+                        }
                     }
                 }
             )
